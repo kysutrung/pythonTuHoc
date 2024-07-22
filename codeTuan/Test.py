926585d354e5ac7f648e7f12cff472a4843ce6ea
Luu Quang Tuan <luuquangtuan24062004@gmail.com>
2024-07-22 22:54:17 +0700

    hoc het bai 8

---
+++ codeTuan/Test.py
@@ -0,0 +1,26 @@
+#Bai 6
+"""a, b, c = map(int, input('Nhap lan luot tuoi bo, tuoi me, tuoi con: ').split())
+ditu = b - c
+if ditu > 18:
+    print("Me tron con vuong, ca nha cung vui")
+else:
+    print('Me tron con vuong bo di TU')"""
+#Bai 7
+#Bai 8
+n = 60
+"""if 50 <= n <= 100:
+    print('nam giua 50 va 100')"""
+"""if n % 20 == 0 and n % 30 == 0:
+    print('Thoa man')
+else:
+    print('Deo thoa man')"""
+
+t = int(input("Nhap thang can ktra:"))
+if t == 1 or t == 3 or t == 5 or t == 7 or t == 8 or t == 10 or t == 12 :
+    print('Thang co 31 ngay')
+elif t == 4 or t == 6 or t == 11:
+    print('Thang co 30 ngay')
+elif t == 2:
+    print('Thang co 28 ngay')
+else:
+    print("Du lieu khong dung")

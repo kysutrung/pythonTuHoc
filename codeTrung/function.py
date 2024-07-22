03c00e4e3865caf12a886b0c2fb5931f629a9197
trungpc01 <letterbox.trung@gmail.com>
2024-07-23 00:11:40 +0700

    sua file function

--- codeTrung/function.py
+++ codeTrung/function.py
@@ -6,7 +6,12 @@ def chao_hoi():
 def chuc_mung():
     print("Chuc ban may man lan sau")
 
+def meo():
+    for i in range(1,3):
+        print("meo")
 
+#đây là hàm main giống trong cpp, cái này sẽ chạy đầu tiên và gọi các hàm liên quan
+#hàm nào không được gọi sẽ không chạy
 def main(): 
     chao_hoi()
     chuc_mung()

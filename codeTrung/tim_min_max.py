a4dc17385de5f21a25b4cecbca3d51cb088d4203
trung_pc_l <trungnd2767@gmail.com>
2024-08-03 22:40:04 +0700

    tai len bai tim min max

---
+++ codeTrung/tim_min_max.py
@@ -0,0 +1,19 @@
+day_so = [0, 3, 6, 9, 8, 0, 8, 4]
+
+def tim_min(list_a):
+    min = 0
+    for i in range(0, len(list_a)):
+        if min > list_a[i]:
+            min = list_a[i]
+    print("min = ", min)
+
+def tim_max(list_a):
+    max = 0
+    for i in range(0, len(list_a)):
+        if max < list_a[i]:
+            max = list_a[i]
+    print("max = ", max)
+
+tim_max(day_so)
+tim_min(day_so)
+
